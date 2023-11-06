c62b77f1d4a006eb330aec9feac2ae7957ad17ad
Gaston <gastonallerdev@gmail.com>
2023-11-06 19:34:17 -0300

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header>
-      <div>
-        <h1>Logo</h1>
+    <header className="flex  items-center w-full p-4">
+      <div className=" w-1/6">
+        <h1>Power</h1>
       </div>
-      <nav>
+      <nav className="flex-1">
         <h1>Nav</h1>
       </nav>
     </header>
