aabadc6aa82b5e08b50156f1cb979a2a70fa6fc8
Gaston <gastonallerdev@gmail.com>
2023-11-06 19:34:09 -0300

    edit tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         primary: "#706FE5",
+        secondary: "#EAEAFC",
+        footer: "#2E3E5C",
       },
     },
   },
   plugins: [],
 };
+
