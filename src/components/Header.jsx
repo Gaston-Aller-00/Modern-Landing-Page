7d83cc5df1291594bf9a40896f3922ef35bceee9
Gaston <gastonallerdev@gmail.com>
2023-11-06 19:40:14 -0300

    e

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { IconName } from "react-icons/ri";
+
+
 
 const Header = () => {
   return (
     <header className="flex  items-center w-full p-4">
-      <div className=" w-1/6">
-        <h1>Power</h1>
+      <div className=" w-1/6 text-center">
+        <h1 className="text-2xl font-bold">Power</h1>
       </div>
-      <nav className="flex-1">
+      <nav className="flex-1 text-center">
         <h1>Nav</h1>
       </nav>
     </header>
