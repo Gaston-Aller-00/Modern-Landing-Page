2a47279d877c4eb2acf45653d99b0d8065b75819
Gaston <gastonallerdev@gmail.com>
2023-11-07 19:21:55 -0300

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,33 @@
-import React,{useState} from "react";
-import { RiCheckboxBlankCircleFill,RiMenu3Fill,RiCloseLine } from "react-icons/ri";
+import React, { useState } from "react";
+import {
+  RiCheckboxBlankCircleFill,
+  RiMenu3Fill,
+  RiCloseLine,
+} from "react-icons/ri";
 
 const Header = () => {
-
-  const [showMenu,setShowMenu] = useState(false)
-  const [dos, setDos] = useState('2')
+  const [showMenu, setShowMenu] = useState(false);
+  const [dos, setDos] = useState("2");
   return (
     // Power, home,about,services,profucts,etc high page part
-    <header className="flex  items-center justify-between xl:justify-start w-full p-4 h-[10vh] z-50">
-      <div className=" xk:w-1/6 text-center">
+    <header className="flex  items-center justify-between xl:justify-start w-full py-4 px-8 h-[10vh] z-50">
+      <div className=" xk:w-1/6 text-center -mt-4">
         {/* icono de la bolita cortada , esta tapado con el bg-white , no esta cortado */}
-        <a href="#" className="text-2xl font-bold relative p-0 bg-white">
+        <a href="#" className="text-2xl font-bold relative p-0 bg-white ">
           Power<span className=" text-primary text-5xl">.</span>
           <RiCheckboxBlankCircleFill className="absolute -left-3 -bottom-3 text-primary -z-10" />
         </a>
       </div>
 
       {/* nav con todos centrado */}
-      <nav className={` fixed  bg-white w-[80%] xl:w-full h-full ${showMenu ? "left-0" : "-left-full"}
- top-0 xl:static flex-1  flex flex-col xl:flex-row items-center justify-center gap-10 transition-all duration-300`} >
+      <nav
+        className={` fixed  bg-white w-[80%] md:w-[%40] xl:w-full h-full ${
+          showMenu ? "left-0" : "-left-full"
+        }
+ top-0 xl:static flex-1  flex flex-col xl:flex-row items-center justify-center gap-10 transition-all duration-300`}
+      >
         {/* boton cruz */}
-        <button onClick={() => setShowMenu(false)}>
+        <button onClick={() => setShowMenu(false)} className=" xl:hidden text-2xl p-2">
           <RiCloseLine />
         </button>
         <a href="#" className="">
@@ -37,8 +44,10 @@ const Header = () => {
         </a>
       </nav>
       <button onClick={() => setShowMenu(!showMenu)} className="text-2xl p-2">
-<RiMenu3Fill/></button>
-
+       
+        {/* si esta true muestra las cruz y si esta false muestra el menu */}
+        {showMenu ? <RiCloseLine /> : <RiMenu3Fill />}
+      </button>
     </header>
   );
 };
