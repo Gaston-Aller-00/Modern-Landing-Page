2a7cc89f09274e6dad1921b50a54ac6ff7d21375
Gaston <gastonallerdev@gmail.com>
2023-11-08 19:55:42 -0300

    finish Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { RiCheckboxBlankCircleFill, RiPlayFill } from "react-icons/ri";
+import {
+  RiCheckboxBlankCircleFill,
+  RiPlayFill,
+  RiStarFill,
+} from "react-icons/ri";
 
 const Hero = () => {
   return (
@@ -36,13 +40,61 @@ const Hero = () => {
       {/* imagen de la chicaa */}
       <div className=" md:col-span-3 flex items-center justify-center relative">
         <div>
-
-        <img
-          src="hero.png"
-          className="w-[250px] h-[250px] md:w-[450px] md:h-[450px] object-cover xl:-mt-28"
-        />
+          <img
+            src="hero.png"
+            className="w-[250px] h-[250px] md:w-[450px] md:h-[450px] object-cover xl:-mt-28"
+          />
+          <div className="relative bg-white shadow-xl rounded-lg p-4 flex flex-col justify-center gap-2 max-w-[250px] mx-auto -mt-12">
+            <div className="flex items-center">
+              <img
+                src="https://img.freepik.com/foto-gratis/empresaria-confiada-sonriente-que-presenta-brazos-cruzados_1262-20950.jpg"
+                className="w-10 h-10 object-cover rounded-full ring-2 ring-gray-300"
+              />
+              <img
+                src="https://img.freepik.com/foto-gratis/hombre-barbudo-feliz-sorprendido-camisa-apuntando-lejos_171337-5021.jpg"
+                className="w-10 h-10 object-cover rounded-full ring-2 ring-gray-300 -ml-4"
+              />
+              <img
+                src="https://img.freepik.com/foto-gratis/retrato-hombre-afroamericano-inteligente-profesional-pie-manos-cruzadas-sobre-pecho-pose-confianza_176420-33861.jpg"
+                className="w-10 h-10 object-cover rounded-full ring-2 ring-gray-300 -ml-4"
+              />
+              <img
+                src="https://img.freepik.com/foto-gratis/feliz-joven_1098-20869.jpg"
+                className="w-10 h-10 object-cover rounded-full ring-2 ring-gray-300 -ml-4"
+              />
+              <img
+                src="https://img.freepik.com/foto-gratis/negocios-finanzas-empleo-concepto-mujeres-emprendedoras-exitosas-joven-empresaria-segura-anteojos-mostrando-gesto-pulgar-arriba-sostenga-computadora-portatil-garantice-mejor-calidad-servicio_1258-59118.jpg"
+                className="w-10 h-10 object-cover rounded-full ring-2 ring-gray-300 -ml-4"
+              />
+            </div>
+            <h2 className="text-xl font-bold tracking-[1px] text-gray-800">
+              120 + Employees
+            </h2>
+            <div className="flex items-center gap-2 text-lg text-gray-500">
+              <RiStarFill className="text-primary" /> 5.0 (3.1k Reviews)
+            </div>
+            <div className="absolute -right-12 -bottom-12 -z-10">
+              <div className="relative">
+                <RiCheckboxBlankCircleFill className="text-primary text-8xl" />
+                <div className="absolute left-0 top-0 bg-white w-14 h-14"></div>
+              </div>
+            </div>
+          </div>
         </div>
         <RiCheckboxBlankCircleFill className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 text-[380px] text-secondary p-[] bg-primary rounded-full -z-10" />
+        {/* Logos */}
+        <img
+          src="figma.png"
+          className="w-10 h-10 md:w-20 md:h-20 object-cover rounded-full border-l-8 border-gray-600 absolute top-[12%] right-[20%] xl:right-[10%]"
+        />
+        <img
+          src="adobe.png"
+          className="w-10 h-10 md:w-20 md:h-20 object-cover rounded-full border-l-8 border-gray-600 absolute top-[10%] xl:top-[2%] left-[20%] xl:left-[10%]"
+        />
+        <img
+          src="sketch.png"
+          className="w-10 h-10 md:w-20 md:h-20 object-cover rounded-full absolute bottom-[5%] left-[15%] xl:left-[3%] -rotate-12"
+        />
       </div>
     </section>
   );
