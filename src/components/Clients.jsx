c01bc1e8a7df426ef5f5b72ddf091e8c2b572985
Gaston <gastonallerdev@gmail.com>
2023-11-08 20:43:43 -0300

    edit

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Clients = () => {
   return (
-    <div className="bg-gray-100 p-8 flex flex-col items-center justify-center gap-8">
-      <h1 className="text-2xl font-medium text-gray-800">Trusted By greatest companies</h1>
-      <div className="flex items-center gap-20">
+    <div className="bg-gray-100 p-8 flex flex-col items-center justify-center gap-8 mt-20 xl:mt-0">
+      <h1 className="text-2xl font-medium text-gray-800 text-center">Trusted By greatest companies</h1>
+      <div className="flex flex-col md:flex-row items-center flex-wrap gap-20">
         <img src="google.png" className="w-40" />
         <img src="airbnb.png" className="w-40" />
         <img src="amazon.png" className="w-40" />
