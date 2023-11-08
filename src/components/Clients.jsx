0bda1dd8c9e7f8311ae3bcb49f899546fa8d4e40
Gaston <gastonallerdev@gmail.com>
2023-11-08 20:23:30 -0300

    semi finish

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react";
 
- const Clients = () => {
+const Clients = () => {
   return (
-    <div>Clients</div>
-  )
-}
+    <div className="bg-gray-100 p-8 flex flex-col items-center justify-center gap-8">
+      <h1 className="text-2xl font-medium text-gray-800">Trusted By greatest companies</h1>
+      <div className="flex items-center gap-20">
+        <img src="google.png" className="w-40" />
+        <img src="airbnb.png" className="w-40" />
+        <img src="amazon.png" className="w-40" />
+        <img src="shopify.svg" className="w-40" />
+        <img src="elektra.svg" className="w-40" />
+      </div>
+    </div>
+  );
+};
 
-
-export default Clients 
+export default Clients;
