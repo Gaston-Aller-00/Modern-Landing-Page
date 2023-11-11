5c4b249863c67c885b03a9c7a0d6775a94f21a62
Gaston <gastonallerdev@gmail.com>
2023-11-10 21:06:40 -0300

    Finish works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Works = () => {
   return (
-    <div className="p-20">
+    <div className="p-8 xl:p-20">
       <div className="mb-8">
         <h1 className="text-[40px] font-bold text-gray-500">
           We create World-CLass Digital Products
@@ -26,24 +26,24 @@ const Works = () => {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2">
           <div>
-            <img src="work2.png" className="w-full rounded-3xl" />
-            <p className="text-gray-500"> App desing -June 22 , 2022</p>
+            <img src="work2.png" className="w-full h-56 object-cover rounded-3xl p-4" />
+            <p className="text-gray-500"> App desing -June 22 , 2023</p>
             <h3 className="text-2xl font-bold">Redesing channel website landing page</h3>
           </div>
           <div>
-            <img src="work2.png" className="w-full rounded-3xl" />
-            <p className="text-gray-500"> App desing -June 22 , 2022</p>
-            <h3 className="text-2xl font-bold">Redesing channel website landing page</h3>
+            <img src="work3.png" className="w-full h-56 object-cover rounded-3xl  p-4" />
+            <p className="text-gray-500"> App desing -June 26 , 2023</p>
+            <h3 className="text-2xl font-bold">New locator App for a New Company</h3>
           </div>
           <div>
-            <img src="work2.png" className="w-full rounded-3xl" />
+            <img src="work4.png" className="w-full h-56 object-cover rounded-3xl p-4" />
             <p className="text-gray-500"> App desing -June 22 , 2022</p>
-            <h3 className="text-2xl font-bold">Redesing channel website landing page</h3>
+            <h3 className="text-2xl font-bold">Rental Rooms web app platform</h3>
           </div>
           <div>
-            <img src="work2.png" className="w-full rounded-3xl" />
+            <img src="work5.png" className="w-full h-56 object-cover rounded-3xl p-4" />
             <p className="text-gray-500"> App desing -June 22 , 2022</p>
-            <h3 className="text-2xl font-bold">Redesing channel website landing page</h3>
+            <h3 className="text-2xl font-bold">Calendar app for Big SASS Company</h3>
           </div>
         </div>
       </div>
