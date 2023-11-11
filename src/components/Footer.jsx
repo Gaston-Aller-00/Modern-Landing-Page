abd86ca717f97f66fd065540d0d3fbc69d4232ea
Gaston <gastonallerdev@gmail.com>
2023-11-11 19:24:23 -0300

    0.9

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,32 +20,90 @@ const Footer = () => {
           <RiCheckboxBlankCircleFill className="absolute -left-3 -bottom-3 text-primary  -z-10" />
         </a>
         {/* redes sociales  */}
-      <nav className="flex items-center justify-center gap-6">
-        <a href="" className=" block text-2xl text-white bg-primary p-4 rounded-full"><RiInstagramLine/></a>
-        <a href="" className=" block text-2xl text-white bg-primary p-4 rounded-full"><RiTwitterLine/></a>
-        <a href="https://github.com/Gaston-Aller-00" className=" block text-2xl text-white bg-primary p-4 rounded-full"><RiGithubLine/></a>
-        <a href="" className=" block text-2xl text-white bg-primary p-4 rounded-full"><RiFacebookBoxLine/></a>
-      </nav>
+        <nav className="flex items-center justify-center gap-6">
+          <a
+            href=""
+            target="_blank"
+            className=" block text-2xl text-white bg-primary p-4 rounded-full"
+          >
+            <RiInstagramLine />
+          </a>
+          <a
+            href=""
+            target="_blank"
+            className=" block text-2xl text-white bg-primary p-4 rounded-full"
+          >
+            <RiTwitterLine />
+          </a>
+          <a
+            href="https://github.com/Gaston-Aller-00"
+            target="_blank"
+            className=" block text-2xl text-white bg-primary p-4 rounded-full"
+          >
+            <RiGithubLine />
+          </a>
+          <a
+            href=""
+            target="_blank"
+            className=" block text-2xl text-white bg-primary p-4 rounded-full"
+          >
+            <RiFacebookBoxLine />
+          </a>
+        </nav>
       </div>
       <div className="mt-2">
-        <h3 className="text-lg font-bold text-white text-center md:text-left">Company</h3>
+        <h3 className="text-lg font-bold text-white text-center md:text-left">
+          Company
+        </h3>
         <nav className="mt-4 flex items-center justify-between gap-8">
-            <a href="#" className="text-gray-300 mt-4 hover:text-white transition-color">About</a>
-            <a href="#" className="text-gray-300 mt-4 hover:text-white transition-color">Press</a>
-            <a href="#" className="text-gray-300 mt-4 hover:text-white transition-color">Investors</a>
-            <a href="#" className="text-gray-300 mt-4 hover:text-white transition-color">Events</a>
-            <a href="#" className="text-gray-300 mt-4 hover:text-white transition-color">Terms of use</a>
-            <a href="#" className="text-gray-300 mt-4 hover:text-white transition-color">Privacy policy</a>
-            <button
-              type="button"
-              className=" font-semibold py-2 px-6 bg-primary text-white rounded-xl "
-            >
-              Contact us
-            </button>
+          <a
+            href="#"
+            className="text-gray-300 mt-4 hover:text-white transition-color"
+          >
+            About
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 mt-4 hover:text-white transition-color"
+          >
+            Press
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 mt-4 hover:text-white transition-color"
+          >
+            Investors
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 mt-4 hover:text-white transition-color"
+          >
+            Events
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 mt-4 hover:text-white transition-color"
+          >
+            Terms of use
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 mt-4 hover:text-white transition-color"
+          >
+            Privacy policy
+          </a>
+          <button
+            type="button"
+            className=" font-semibold py-2 px-6 bg-primary text-white rounded-xl "
+          >
+            Contact us
+          </button>
         </nav>
       </div>
       <div>
-        <p className=" flex  justify-center items-center p-8 text-gray-400">© GastonDev 2023 - All Rights Reserved</p>
+        <p className=" flex  justify-center items-center p-8 text-gray-400">
+          © GastonDev 2023 - All Rights Reserved
+        </p>
       </div>
     </footer>
   );
