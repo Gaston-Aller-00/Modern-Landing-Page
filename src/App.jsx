c743b24fcbaa75f340ae4360c42f42399ee5a214
Gaston <gastonallerdev@gmail.com>
2023-12-08 23:42:22 -0300

    fix A

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import Clients from "./components/Clients"
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import  Hero  from "./components/Hero"
-import Reviews from "./components/Reviews"
-import Services from "./components/Services"
-import Testimonios from "./components/Testimonios"
-import Works from "./components/Works"
+import { BrowserRouter as Router } from "react-router-dom";
+import Clients from "./components/Clients";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Reviews from "./components/Reviews";
+import Services from "./components/Services";
+import Testimonios from "./components/Testimonios";
+import Works from "./components/Works";
 
 function App() {
-
   return (
     <>
-      <div >
-        <Header/>
-        <Hero/>
-        <Clients/> 
-        <Works/>
-        <Reviews/>
-        <Testimonios/>
-        <Services/>
-        <Footer/>
+      <div>
+        <Router>
+          <Header />
+          <Hero />
+          <Clients />
+          <Works />
+          <Reviews />
+          <Testimonios />
+          <Services />
+          <Footer />
+        </Router>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import {
   RiCheckboxBlankCircleFill,
   RiMenu3Fill,
@@ -10,45 +11,49 @@ const Header = () => {
   const [dos, setDos] = useState("2");
   return (
     // Power, home,about,services,profucts,etc high page part
-    <header className="flex  items-center justify-between xl:justify-start w-full py-4 px-8 h-[10vh] z-50">
-      <div className=" xk:w-1/6 text-center -mt-4">
-        {/* icono de la bolita cortada , esta tapado con el bg-white , no esta cortado */}
-        <a href="#" className="text-2xl font-bold relative p-0 bg-white ">
-          Power<span className=" text-primary text-5xl">.</span>
-          <RiCheckboxBlankCircleFill className="absolute -left-3 -bottom-3 text-primary -z-10" />
-        </a>
-      </div>
 
-      {/* nav con todos centrado */}
-      <nav
-        className={` fixed  bg-white w-[80%] md:w-[%40] xl:w-full h-full ${
+    <>
+      <header className="flex items-center justify-between xl:justify-start w-full py-4 px-8 h-[10vh] z-50">
+        <div className=" xk:w-1/6 text-center -mt-4">
+          {/* icono de la bolita cortada , esta tapado con el bg-white , no esta cortado */}
+          <Link href="#" className="text-2xl font-bold relative p-0 bg-white ">
+            Power<span className=" text-primary text-5xl">.</span>
+            <RiCheckboxBlankCircleFill className="absolute -left-3 -bottom-3 text-primary -z-10" />
+          </Link>
+        </div>
+
+        {/* nav con todos centrado */}
+        <nav
+        className={`fixed bg-white w-[80%] md:w-[%40] xl:w-full ${
           showMenu ? "left-0" : "-left-full"
-        }
- top-0 xl:static flex-1  flex flex-col xl:flex-row items-center justify-center gap-10 transition-all duration-300`}
+        } top-0 xl:static flex-1 flex flex-col xl:flex-row items-center justify-center gap-10 transition-all duration-300`}
       >
-        {/* boton cruz */}
-        <button onClick={() => setShowMenu(false)} className=" xl:hidden text-2xl p-2">
+        <button
+          onClick={() => setShowMenu(false)}
+          className="xl:hidden text-2xl p-2"
+        >
           <RiCloseLine />
         </button>
-        <a href="#" className="">
+        <Link to="/" className="text-xl hover:text-purple-500 transition-colors duration-200 ">
           Home
-        </a>
-        <a href="#AboutUs" className="">
-          About us{" "}
-        </a>
-        <a href="#services" className="">
+        </Link>
+        <Link to="#AboutUs" className="text-xl hover:text-purple-500 transition-colors duration-200 ">
+          About us
+        </Link>
+        <Link to="#services" className="text-xl hover:text-purple-500 transition-colors duration-200 ">
           Services
-        </a>
-        <a href="#reviews" className="">
+        </Link>
+        <Link to="#reviews" className="text-xl hover:text-purple-500 transition-colors duration-200 ">
           Products
-        </a>
+        </Link>
       </nav>
-      <button onClick={() => setShowMenu(!showMenu)} className="text-2xl p-2">
-       
-        {/* si esta true muestra las cruz y si esta false muestra el menu */}
-        {/* {showMenu ? <RiCloseLine /> : <RiMenu3Fill />} */}
-      </button>
-    </header>
+
+        <button onClick={() => setShowMenu(!showMenu)} className="text-2xl p-2">
+          {/* si esta true muestra las cruz y si esta false muestra el menu */}
+          {/* {showMenu ? <RiCloseLine /> : <RiMenu3Fill />} */}
+        </button>
+      </header>
+    </>
   );
 };
 
